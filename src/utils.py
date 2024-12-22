1cb74c417c7c1b78e9f816bb99918baea0cc590a
Felipe <56170578+felipbizz@users.noreply.github.com>
2024-12-22 11:04:42 -0300

    Initial commit

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+import torch
+from typing import Union
+
+
+def wmape(y_true, y_pred):
+    return np.abs(y_true - y_pred).sum() / np.abs(y_true).sum()
+
+# --------------------------- 
+
+class WMAPE(torch.nn.Module):
+
+    def __init__(self):
+        super(WMAPE, self).__init__()
+        self.outputsize_multiplier = 1
+        self.output_names = [""]
+        self.is_distribution_output = False
+
+    def domain_map(self, y_hat: torch.Tensor):
+        return y_hat.squeeze(-1)
+
+    def __call__(
+        self,
+        y: torch.Tensor,
+        y_hat: torch.Tensor,
+        mask: Union[torch.Tensor, None] = None,
+    ):
+        if mask is None:
+            mask = torch.ones_like(y_hat)
+
+        num = mask * (y - y_hat).abs()
+        den = mask * y.abs()
+        return num.sum() / den.sum()
