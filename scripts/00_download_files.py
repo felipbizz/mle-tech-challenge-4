e11b138da8cbf4253632bf7449e09797b4f09573
eng-alecrim <lucasalecrimvieira@gmail.com>
2025-01-10 15:57:16 -0300

    pequena alteração na lógica e local destino do deltalake

--- scripts/00_download_files.py
+++ scripts/00_download_files.py
@@ -1,9 +1,29 @@
 from tqdm import tqdm
 import yfinance as yf
 from deltalake.writer import write_deltalake
+from src.utils import get_path_project
+
+PROJECT_DIR = get_path_project()
+DATALAKE_PATH = PROJECT_DIR / "data/raw/yfinance_api"
+SYMBOLS = [
+    "DIS",
+    "VALE3",
+    "PETR4",
+    "ITUB4",
+    "ABEV3",
+    "BBDC4",
+    "SANB11",
+    "BBAS3",
+    "JBSS3",
+    "KLBN11",
+    "BPAC11",
+    "BBDC3",
+    "ITSA4",
+    "WEGE3",
+]
 
 
-def download_files(symbols: list):
+def download_files(symbols: list) -> None:
     for symbol in tqdm(symbols):
         print(f"Processando {symbol}")
         try:
@@ -17,29 +37,24 @@ def download_files(symbols: list):
         df.columns = df.columns.droplevel(1)
         df = df.reset_index()
 
-        df = df.rename(columns={"Date": "ds", "Close": "y", }) #"Price": "DIS"
+        df = df.rename(
+            columns={
+                "Date": "ds",
+                "Close": "y",
+            }
+        )  # "Price": "DIS"
         df = df[["ds", "y"]]
 
         df["unique_id"] = symbol
-        write_deltalake("deltalake", df, mode="append", partition_by=["unique_id"])
+        write_deltalake(DATALAKE_PATH, df, mode="append", partition_by=["unique_id"])
+
+    return None
+
+
+def main() -> None:
+    download_files(SYMBOLS)
+    return None
 
 
 if __name__ == "__main__":
-    symbols = [
-        "DIS",
-        "VALE3",
-        "PETR4",
-        "ITUB4",
-        "ABEV3",
-        "BBDC4",
-        "SANB11",
-        "BBAS3",
-        "JBSS3",
-        "KLBN11",
-        "BPAC11",
-        "BBDC3",
-        "ITSA4",
-        "WEGE3",
-    ]
-
-    download_files(symbols)
+    main()
